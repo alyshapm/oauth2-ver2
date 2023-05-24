7dab14a50faa2fe273e3d905da2d5ae13ad89ba8
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-05-24 23:45:01 +0700

    added strings

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react'
+
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 function Landing() {
-    const strings = ['Did you do the laundry today?', 'Did you leave the stove on?', 'YOUR NAME IS IDA BAGUS KERTHYAYANA MANAUBA RIGHT', 'Backend amirite sir ahahahaha', 'I am thou, thou art I. Thou hast acquired a new vow. It shall become the wings of rebilllon that breakth thy chains of captivty.', 'Sidon: Smash or pass?', 'Fish can roll', '99% of gamblers quit before they hit big', 'Backend developer', 'SIUUUUUUUUUUUUUUUUUUUUUU', 'Messi or Ronaldo?', 'My greatest pain in life is that I will never be able to see myself perform live. ― Kanye West', 'I AM A SURGEON I AM A SURGEON DOCTOR HAN', ];
+    const strings = ['Did you do the laundry today?',
+    'Did you leave the stove on?',
+    'YOUR NAME IS IDA BAGUS KERTHYAYANA MANAUBA RIGHT',
+    'Backend amirite sir ahahahaha',
+    'I am thou, thou art I. Thou hast acquired a new vow. It shall become the wings of rebilllon that breakth thy chains of captivty.', 
+    'Sidon: Smash or pass?', 
+    'Fish can roll', 
+    '99% of gamblers quit before they hit big', 
+    'Backend developer', 
+    'SIUUUUUUUUUUUUUUUUUUUUUU', 
+    'Messi or Ronaldo?', 
+    'My greatest pain in life is that I will never be able to see myself perform live. ― Kanye West', 
+    'I AM A SURGEON I AM A SURGEON DOCTOR HAN',
+    'Dont you mean sturgeon?',
+    'I AM FYPL',
+    'Do you remember? The 21st night of September',
+    'Best RE Protag: Leon or Ethan?'
+    ];
+
     const [randomValue, setRandomValue] = useState(null);
 
     useEffect(() => {
@@ -18,7 +37,7 @@ function Landing() {
     };
   
   return (
-    <div className=''>
+    <div className='landing'>
         <h1>Welcome User!</h1>
         <h3>{randomValue}</h3>
         <Link to="/">
