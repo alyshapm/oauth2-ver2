258812606ad9e87356b6e3da80f2516ecddbf433
gloxiinia <91533469+gloxiinia@users.noreply.github.com>
2023-05-25 13:04:14 +0700

    Login to landing page works

--- frontend/src/components/InsertField.jsx
+++ frontend/src/components/InsertField.jsx
@@ -7,57 +7,41 @@ import '../styles/ifield.css'
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import { useNavigate } from "react-router";
-import { fetchToken, setToken } from "../Auth";
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 
 function InsertField() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const login = () => {
-    if ((username == "") & (password == "")) {
-      return;
-    } else {
-      // make api call to our backend. we'll leave thisfor later
-      axios
-        .post("http://localhost:8000/login", {
-          username: username,
-          password: password,
-        })
-        .then(function (response) {
-          console.log(response.data.token, "response.data.token");
-          if (response.data.token) {
-            setToken(response.data.token);
-            navigate("/landing");
-          }
-        })
-        .catch(function (error) {
-          console.log(error, "error");
-        });
+  const login = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    try {
+      const response = await axios.post('http://localhost:8000/login', { username, password }, { headers: { 'content-type': 'application/x-www-form-urlencoded;charset=UTF-8' } });
+      const token = response.data.access_token; // Assuming the token field is "access_token"
+      localStorage.setItem('token', token);
+      navigate('/landing'); // Redirect to the profile page after successful login
+    } catch (error) {
+      console.error(error.response.data);
     }
   };
 
   return (
     <div className="insertfield">
-       {fetchToken()? (
-          <h3>You are logged in</h3>
-        ):(
-          <Box 
-            sx={{ width: '100%' }}
-          >
-            <h1>Login</h1>
-            <h3>Welcome! Login to your account to get started.</h3>
-          
-            <Stack spacing={2}>
-              <TextField id="username" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} />
-              <TextField id="password" label="Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
-              <Button variant="contained" sx={{ width: '100%' }} onClick={login}>Login</Button>
-            </Stack>
-            <p>New user? <a href='/'>Sign up</a></p>
-          </Box>
-         )}
+      <form onSubmit={login}> {/* Add form element and set onSubmit attribute */}
+        <Box sx={{ width: '100%' }}>
+          <h1>Login</h1>
+          <h3>Welcome! Login to your account to get started.</h3>
+        
+          <Stack spacing={2}>
+            <TextField id="username" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} />
+            <TextField id="password" label="Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
+            <Button variant="contained" sx={{ width: '100%' }} type="submit">Login</Button>
+          </Stack>
+          <p>New user? <Link to="/">Sign up</Link></p> {/* Use React Router's Link component */}
+        </Box>
+      </form>
     </div>
   )
 }
